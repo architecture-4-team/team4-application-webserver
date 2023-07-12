fd46a71c49edf376bcd6a8b141acf9c89b8af353
sunmi.hong <sunmi.hong@lge.com>
2023-07-12 17:25:21 +0900

    Add conference call and bug fixs

--- public/js/contactList.js
+++ public/js/contactList.js
@@ -19,6 +19,7 @@ function initUI() {
     $("#searchUserinfo").val('');
     $("#favoriteUserListDiv > table > tbody").text('');
     $("#searchUserListDiv > table > tbody").text('');
+    $("#reservationListDiv > table > tbody").text('');
 
     $(".favoriteUserDiv").show();
     $(".reservationUserDiv").show();
@@ -76,7 +77,11 @@ function getFavoriteUserList() {
             success: function (data) {
                 console.log("ajax success.. : " + JSON.stringify(data));
 
-                getSearchUser(data);
+                if (data.result != 'ok') {
+                    alert("Fail to get UserInfo from server");
+                } else {
+                    getSearchUser(data);
+                }
             }
         });
     });
@@ -112,10 +117,16 @@ function getFavoriteUserList() {
             },
             success: function (data) {
                 console.log("ajax success.. : " + JSON.stringify(data));
-                alert("success to add contactList");
 
-                $("#nickname").val('');
-                getFavoriteUserList();
+                if (data.result != 'ok') {
+                    alert("Fail to add contactList from server");
+                } else {
+                    alert("success to add contactList");
+
+                    $("#nickname").val('');
+                    $("#searchUserinfo").val('');
+                    getFavoriteUserList();
+                }
             }
         });
     });
@@ -162,23 +173,30 @@ function getFavoriteUserList() {
     });
 
     $("#cancelBtn").unbind().bind("click", function () {
-        location.replace("http://" + location.hostname + ":3000/contactList");
+        $("#nickname").val('');
+        $("#searchUserinfo").val('');
+
+        $(".favoriteUserDiv").show();
+        $(".reservationUserDiv").show();
+        $(".searchUserDiv").hide();
     });
 }
 
 function drawContactList(data) {
+    $("#favoriteUserListDiv > table").css('display', 'none');
+    $("#favoriteUserListDiv > table > tbody").text('');
+
     if (data.contents.length == undefined) {
         $("#favoriteUserListDiv").addClass('noData');
         $("#favoriteUserListDiv > table").attr('display', true);
         return;
     } else {
-        $("#favoriteUserListDiv > table > tbody").text('');
         var html = "";
         for (var i = 0; i < data.contents.length; i++) {
             html += '<tr> \
-                        <td style="width:150px;"><input type="checkbox" id="chk_' + i + '" name="checkbox" uuid="' + data.contents[i].favorite_uid.uuid + '" email="' + data.contents[i].favorite_uid.email + '"> \
+                        <td><input type="checkbox" id="chk_' + i + '" name="checkbox" uuid="' + data.contents[i].favorite_uid.uuid + '" email="' + data.contents[i].favorite_uid.email + '"> \
                         <label for="chk_' + i + '">' + data.contents[i].nickname + '</td> \
-                        <td><button type="button" class="button dark mini fw delFavoriteUser" style="width:50px;" uuid="' + data.contents[i].favorite_uid.uuid + '" >DEL</button></td> \
+                        <td><button type="button" class="button dark mini fw delFavoriteUser" style="width:50px; float: right;" uuid="' + data.contents[i].favorite_uid.uuid + '" >DEL</button></td> \
                     </tr>';
         }
 
@@ -207,9 +225,13 @@ function drawContactList(data) {
                 },
                 success: function (data) {
                     console.log("ajax success.. : " + JSON.stringify(data));
-                    alert("success to delete contactList");
 
-                    getFavoriteUserList();
+                    if (data.result != 'ok') {
+                        alert("Fail to delete contactList from server");
+                    } else {
+                        alert("success to delete contactList");
+                        getFavoriteUserList();
+                    }
                 }
             });
         });
@@ -217,18 +239,19 @@ function drawContactList(data) {
 }
 
 function getSearchUser(data) {
+    $("#searchUserListDiv > table").css('display', 'none');
+    $("#searchUserListDiv > table > tbody").text('');
+
     if (data.result == "nok" || data.contents.length == 0) {
         alert("No searched User.");
         return;
     } else {
-        $("#searchUserListDiv > table > tbody").text('');
-
         // TODO 내 검색 미포함 처리하기
         var html = "";
         for (var i = 0; i < data.contents.length; i++) {
             html += '<tr> \
-                    <td><input type="radio" id="chk_' + i + '" name="radio" uuid="' + data.contents[i].uuid + '" email="' + data.contents[i].email + '" > \
-                    <label for="chk_' + i + '">' + data.contents[i].email + ", " + data.contents[i].firstname + " " + data.contents[i].lastname + '</td> \
+                    <td><input type="radio" id="radio_' + i + '" name="radio" uuid="' + data.contents[i].uuid + '" email="' + data.contents[i].email + '" > \
+                    <label for="radio_' + i + '">' + data.contents[i].email + ", " + data.contents[i].firstname + " " + data.contents[i].lastname + '</td> \
                 </tr>';
         }
 
@@ -243,15 +266,68 @@ function getSearchUser(data) {
 }
 
 function getReservationList() {
-    var html = "";
-    for (var i = 0; i < 4; i++) {
-        html += '<tr> \
-                    <td style="width:100px;" >Reservation Meeting ( 23.07.1' + i + ' 09:30 AM )</td> \
-                    <td><button type="button" class="button dark mini fw" style="width:50px;" >JOIN</button></td> \
-                </tr>';
-    }
+    $.ajax({
+        type: "GET",
+        url: SERVER_IP + "api/schedule",
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        crossDomain: true,
+        dataType: "json",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", uuid);
+        },
+        error: function (data) {
+            console.log('ajax fail.. :' + JSON.stringify(data));
+        },
+        success: function (data) {
+            console.log("ajax success.. : " + JSON.stringify(data));
+            drawReservationList(data);
+        }
+    });
+}
 
-    $("#reservationListDiv").removeClass('noData');
-    $("#reservationListDiv > table").css('display', 'inline-table');
-    $("#reservationListDiv > table > tbody").append(html);
+function drawReservationList(data) {
+    $("#reservationListDiv > table").css('display', 'none');
+    $("#reservationListDiv > table > tbody").text('');
+
+    if (data.contents.length == undefined) {
+        $("#reservationListDiv").addClass('noData');
+        $("#reservationListDiv > table").attr('display', true);
+        return;
+    } else {
+        var html = "";
+        for (var i = 0; i < data.contents.length; i++) {
+            var time = new Date(data.contents[i].updated_at);
+            var now = new Date();
+
+            // TODO 시작시간 1일 이내의 미팅만 JOIN 버튼 활성화
+            console.log(now.getTime())
+            console.log(time.getTime())
+
+            var active = false;
+            if (now.getTime() >= time.getTime() && now.getTime() < (time.getTime() + 86500000))
+                active = true;
+
+            console.log(active)
+            
+            html += '<tr> \
+                        <td>' + "[ Reservation ID : " + data.contents[i].room_id + " ] " + time.getFullYear() + "-" + (time.getMonth() + 1) + "-" + time.getDate() + " " + time.getHours() + ":" + time.getMinutes() + '</td> \
+                        <td><button type="button" class="button dark mini fw joinMeeting" style="width:50px; float: right;" roomid=" ' + data.contents[i].room_id + '">JOIN</button></td> \
+                    </tr>';
+        }
+
+        $("#reservationListDiv").removeClass('noData');
+        $("#reservationListDiv > table").css('display', 'inline-table');
+        $("#reservationListDiv > table > tbody").append(html);
+
+        $(".joinMeeting").unbind().bind("click", function () {
+            var roomId = $(this).attr('roomid').toString().replace(' ', '');
+            var jsonObj = {
+                action: "join",
+                roomid: roomId
+            }
+            console.log(jsonObj)
+            window.chrome.webview.postMessage(JSON.stringify(jsonObj));
+        });
+    }
 }
